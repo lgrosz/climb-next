46eb6374d966be71a5202fcef5a5a7362559df4b
Logan Grosz <logan.grosz@gmail.com>
2025-01-02 13:48:40 -0700

    refactor: rewrite display portion of singular area page

--- actions.ts
+++ actions.ts
@@ -52,31 +52,6 @@ export async function addClimb(names?: string[], grades?: GradeData[], areaId?: 
   }
 }
  
-export async function removeArea(id: number) {
-  const response = await fetch(GRAPHQL_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query: `mutation { removeArea(id: ${id}) { id }}`
-    })
-  })
-
-  const result = await response.json()
-
-  if (response.ok) {
-    // TODO
-    // - How can I revalidate everything that may reference
-    //   this area??
-    revalidatePath("/areas")
-    revalidatePath(`/area/${id}`)
-  } else {
-    throw(result.errors)
-  }
-}
-
 export async function addArea(names?: String[], superAreaId?: number) {
   const response = await fetch(GRAPHQL_ENDPOINT, {
     method: "POST",
@@ -107,54 +82,6 @@ export async function addArea(names?: String[], superAreaId?: number) {
 
   if (response.ok) {
     let id = result.data.addArea.id
-    revalidatePath("/areas")
-    revalidatePath(`/area/${id}`)
-    return id;
-  } else {
-    throw(result.errors)
-  }
-}
-
-export async function addAreaName(id: number, name: string) {
-  const response = await fetch(GRAPHQL_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query: `mutation { addAreaName(id: ${id}, name: "${name}") { id }}`
-    })
-  })
-
-  const result = await response.json()
-
-  if (response.ok) {
-    let id = result.data.addAreaName.id
-    revalidatePath("/areas")
-    revalidatePath(`/area/${id}`)
-    return id;
-  } else {
-    throw(result.errors)
-  }
-}
-
-export async function removeAreaName(id: number, name: string) {
-  const response = await fetch(GRAPHQL_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query: `mutation { removeAreaName(id: ${id}, name: "${name}") { id }}`
-    })
-  })
-
-  const result = await response.json()
-
-  if (response.ok) {
-    let id = result.data.removeAreaName.id
     revalidatePath("/areas")
     revalidatePath(`/area/${id}`)
     return id;
--- app/area/[id]/page.tsx
+++ app/area/[id]/page.tsx
@@ -1,145 +1,107 @@
-import DeleteAreaButton from '@/components/DeleteAreaButton'
-import AddAreaNameForm from '@/components/AddAreaNameForm'
-import RemoveAreaNameButton from '@/components/RemoveAreaNameButton'
 import { GRAPHQL_ENDPOINT } from '@/constants'
 import Link from 'next/link'
+import { query } from '@/graphql'
 
-async function AreaNameListItem({ areaId, name, children }: { areaId: number, name: string, children: React.ReactNode }) {
-  return (
-    <li>
-      <div>
-        {children}
-        <RemoveAreaNameButton areaId={areaId} name={name}>Remove</RemoveAreaNameButton>
-      </div>
-    </li>
-  )
+interface SubArea {
+  id: number,
+  name: string | null,
+}
+
+interface Formation {
+  id: number,
+  name: string | null,
+}
+
+interface Climb {
+  id: number,
+  name: string | null,
+}
+
+interface AreaParent {
+  __typename: string,
+  id: number,
+  name: string | null,
 }
 
 interface Area {
   id: number,
-  names: string[],
-  superArea: {
-    id: number,
-    names: string[]
-  } | null,
-  subAreas: {
-    id: number,
-    names: string[]
-  }[],
-  formations: {
-    id: number,
-    names: string[]
-  }[],
-  climbs: {
-    id: number,
-    names: string[]
-  }[],
+  name: string | null,
+  areas: SubArea[],
+  formations: Formation[],
+  climbs: Climb[],
+  parent: AreaParent | null,
 }
 
-export default async function Page({ params }: { params: { id: string } }) {
-  let area: Area = await fetch(GRAPHQL_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query: `query GetArea($id: Int!) {
-        area(
-          id: $id
-        ) {
-          id names
-          superArea { id names }
-          subAreas { id names }
-          formations { id names }
-          climbs { id names }
-        }
-      }`,
-      variables: { id: parseInt(params.id) }
-    })
-  })
-    .then(r => r.json())
-    .then(json => json.data.area)
+const dataQuery = `
+  query($id: Int!) {
+    area(
+      id: $id
+    ) {
+      id name
+      areas { id name }
+      formations { id name }
+      climbs { id name }
+      parent {
+        __typename
+        ... on Area { id name }
+      }
+    }
+  }
+`
 
-  // We will treat the official name as the first of the names list
-  const name = area.names.find(Boolean)
+export default async function Page({ params }: { params: { id: string } }) {
+  const result = await query(GRAPHQL_ENDPOINT, dataQuery, { id: parseInt(params.id) })
+    .then(r => r.json());
+  const { data, errors } = result;
+
+  if (errors) {
+    console.error(JSON.stringify(errors, null, 2));
+    return <div>There was an error generating the page.</div>
+  }
+
+  const { area }: { area: Area } = data;
+
+  let parentHref: string | null = null;
+  if (area?.parent?.__typename == "Area") {
+    parentHref = `/areas/${area.parent.id}`
+  }
 
   return (
     <div>
-      <h1>Area <i>{name}</i></h1>
-      <h2>Names</h2>
-      <div>
-        <ul>
-          {area.names.map((name: string, index: number) => (
-            <AreaNameListItem key={index} areaId={area.id} name={name}>{name}</AreaNameListItem>
-          ))}
-          <li>
-            <AddAreaNameForm areaId={area.id} />
-          </li>
-        </ul>
-      </div>
-      <h2>Super Area</h2>
-      {area.superArea ?
-        <Link href={`/area/${area.superArea.id}`}>{area.superArea.names.find(Boolean) ?? "Unnamed"}</Link>
-        :
-        "No ancestor"
+      {
+        area.name ?
+        <h1>{area.name}</h1> :
+        <h1><i>Unnamed Area</i></h1>
       }
-      <h2>Sub Areas</h2>
+      {
+        area.parent ?
+        <h2><Link href={`${parentHref}`}>{area.parent.name}</Link></h2> :
+        null
+      }
+      <h3>Areas</h3>
       <ul>
-        {area.subAreas.map((subarea) => (
-          <li key={`subarea-${subarea.id}`}>
-            <Link href={`/area/${subarea.id}`}>{subarea.names.find(Boolean) ?? "Unnamed"}</Link>
+        {area.areas.map((area) => (
+          <li key={`area-${area.id}`}>
+            <Link href={`/area/${area.id}`}>{area.name}</Link>
           </li>
         ))}
-        <li>
-          <Link
-            href={{
-              pathname: "/areas/create",
-              query: { "super-area-id": area.id },
-            }}
-          >
-            Create sub area
-          </Link>
-        </li>
       </ul>
-      <h2>Formations</h2>
+      <h3>Formations</h3>
       <ul>
         {area.formations.map((formation) => (
           <li key={`formation-${formation.id}`}>
-            <Link href={`/formation/${formation.id}`}>{formation.names.find(Boolean) ?? "Unnamed"}</Link>
+            <Link href={`/formation/${formation.id}`}>{formation.name}</Link>
           </li>
         ))}
-        <li>
-          <Link
-            href={{
-              pathname: "/formations/create",
-              query: { "area-id": area.id },
-            }}
-          >
-            Create formation
-          </Link>
-        </li>
       </ul>
-      <h2>Climbs</h2>
+      <h3>Climbs</h3>
       <ul>
         {area.climbs.map((climb) => (
           <li key={`climb-${climb.id}`}>
-            <Link href={`/climb/${climb.id}`}>{climb.names.find(Boolean) ?? "Unnamed"}</Link>
+            <Link href={`/climb/${climb.id}`}>{climb.name}</Link>
           </li>
         ))}
-        <li>
-          <Link
-            href={{
-              pathname: "/climbs/create",
-              query: { "area-id": area.id },
-            }}
-          >
-            Create climb
-          </Link>
-        </li>
       </ul>
-      <hr />
-      <DeleteAreaButton areaId={area.id}>Delete <i>{name}</i></DeleteAreaButton>
     </div>
   )
 }
