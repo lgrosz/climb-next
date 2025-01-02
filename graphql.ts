3d4b49713cb7c2cc286412eff6606427c51b527c
Logan Grosz <logan.grosz@gmail.com>
2025-01-02 11:30:37 -0700

    refactor: add graphql helper module
    This module will export helpers to work with GraphQL queries.

---
+++ graphql.ts
@@ -0,0 +1,12 @@
+export function query(endpoint: string, query: string) {
+  return fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: query,
+    })
+  })
+}
