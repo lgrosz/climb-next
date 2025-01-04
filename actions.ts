fb082ad6157c64ae3646d0fd93abf04413471853
Logan Grosz <logan.grosz@gmail.com>
2025-01-04 15:39:01 -0700

    feat: add rename formation server action

--- actions.ts
+++ actions.ts
@@ -204,3 +204,75 @@ export async function addFormation(names?: String[], areaId?: number, superForma
     throw(result.errors)
   }
 }
+
+export async function renameFormation(formationId: number, name: string) {
+  const dataQuery = `
+    mutation(
+      $id: Int!
+      $name: String
+    ) {
+      action: renameFormation(
+        id: $id
+        name: $name
+      ) {
+        name
+        parent {
+          __typename
+          ... on Area {
+            id
+          }
+          ... on Formation {
+            id
+          }
+        }
+        formations { id }
+        climbs { id }
+      }
+    }
+  `;
+
+  const result = await query(GRAPHQL_ENDPOINT, dataQuery, {
+    id: formationId,
+    name: name || null,
+  })
+    .then(r => r.json());
+
+  const { data, errors } = result;
+
+  if (errors) {
+    console.error(JSON.stringify(errors, null, 2));
+  }
+
+  revalidatePath('/table-of-contents')
+  revalidatePath(`/formations/${formationId}`)
+
+  if (data?.action?.parent?.__typename == "Area") {
+    const parentAreaId = data?.action?.parent?.id;
+    if (parentAreaId) {
+      revalidatePath(`/areas/${parentAreaId}`)
+    }
+  } else if (data?.action?.parent?.__typename == "Formation") {
+    const parentFormationId = data?.action?.parent?.id;
+    if (parentFormationId) {
+      revalidatePath(`/formations/${parentFormationId}`)
+    }
+  }
+
+  const childFormations = data?.action?.formations ?? []
+  for (const child of childFormations) {
+    const childFormationId = child?.id
+    if (childFormationId) {
+      revalidatePath(`/formations/${childFormationId}`)
+    }
+  }
+
+  const childClimbs = data?.action?.climbs ?? []
+  for (const child of childClimbs) {
+    const childClimbId = child?.id
+    if (childClimbId) {
+      revalidatePath(`/climbs/${childClimbId}`)
+    }
+  }
+
+  return data?.action?.name ?? "";
+}
