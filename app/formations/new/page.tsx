5347ed8d55073eeabde6c90b92eb4d2003ed2e3b
Logan Grosz <logan.grosz@gmail.com>
2025-01-12 14:44:16 -0700

    feat: add /formations/new route
    ... submission of the form on this page will create a new formation.

---
+++ app/formations/new/page.tsx
@@ -0,0 +1,73 @@
+import Form from 'next/form';
+import { create } from '@/formations/actions';
+import { redirect } from "next/navigation";
+
+export default () => {
+  const action = async (formData: FormData) => {
+    'use server';
+    const name = formData.get('name')?.toString() || null;
+    const parentType = formData.get('parent-type')?.toString() || null;
+    const parentAreaId = Number(formData.get('parent-area-id')?.toString()) || null;
+    const parentFormationId = Number(formData.get('parent-formation-id')?.toString()) || null;
+
+    let id = await create(
+      name ?? undefined,
+      {
+        area: (parentType === "area" ? parentAreaId : null) ?? undefined,
+        formation: (parentType === "formation" ? parentFormationId : null) ?? undefined
+      }
+    );
+
+    redirect(`/formations/${id}`);
+  }
+
+  return (
+    <Form action={action}>
+      <div>
+        <label htmlFor="name">Add a name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Name"
+        />
+      </div>
+      <div>
+        <input
+          id="parent-is-area"
+          type="radio"
+          name="parent-type"
+          value="area"
+        />
+        <label htmlFor="parent-is-area">Parent is an area</label>
+        <input
+          id="parent-is-formation"
+          type="radio"
+          name="parent-type"
+          value="formation"
+        />
+        <label htmlFor="parent-is-formation">Parent is a formation</label>
+      </div>
+      <div>
+        <label htmlFor="parent-area-id">Add a parent area</label>
+        <input
+          id="parent-area-id"
+          name="parent-area-id"
+          type="number"
+          placeholder="Parent area id"
+        />
+      </div>
+      <div>
+        <label htmlFor="parent-formation-id">Add a parent formation</label>
+        <input
+          id="parent-formation-id"
+          name="parent-formation-id"
+          type="number"
+          placeholder="Parent formation id"
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </Form>
+  )
+}
+
