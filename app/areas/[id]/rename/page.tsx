38747ad842ca4f10b7dc85fd469899704a480d39
Logan Grosz <logan.grosz@gmail.com>
2025-01-30 19:46:33 -0700

    feat: add rename area page

---
+++ app/areas/[id]/rename/page.tsx
@@ -0,0 +1,26 @@
+import Form from 'next/form';
+import { rename } from '@/areas/actions';
+import { redirect } from 'next/navigation';
+
+export default async (props: { params: Promise<{ id: string }> }) => {
+  const params = await props.params;
+  const id = parseInt(params.id);
+  const action = async (data: FormData) => {
+    'use server';
+    await rename(id, data.get("name")?.toString() ?? "");
+    redirect(`/areas/${id}`);
+  }
+
+  return (
+    <Form action={action}>
+      <label htmlFor="name">Add a name</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Name"
+      />
+      <button type="submit">Rename</button>
+    </Form>
+  )
+}
