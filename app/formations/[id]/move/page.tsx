e23d3a6bf3b61513db890ab15a675ca070ac792d
Logan Grosz <logan.grosz@gmail.com>
2025-02-04 22:59:41 -0700

    feat: add move-formation page
    Adds move-formation action, page, and link to page from area instance page.

---
+++ app/formations/[id]/move/page.tsx
@@ -0,0 +1,152 @@
+import Form from 'next/form';
+import { move } from '@/formations/actions';
+import { redirect } from 'next/navigation';
+import { GRAPHQL_ENDPOINT } from '@/constants';
+import RadioTree, { RadioTreeNode } from '@/components/RadioTree';
+import { Area, Formation } from '@/graphql/schema';
+import { query } from '@/graphql';
+
+const dataQuery = `
+  query {
+    potentialParentAreas: areas {
+      __typename
+      id name
+      parent {
+        __typename
+        ... on Area { id }
+      }
+    }
+    potentialParentFormations: formations {
+      __typename
+      id name
+      parent {
+        __typename
+        ... on Area { id }
+        ... on Formation { id }
+      }
+    }
+  }
+`
+
+function buildTree(parents: (Area | Formation)[], disabledId: number) {
+  const roots: RadioTreeNode[] = [];
+  const map = new Map<string, RadioTreeNode>();
+
+  // Populate radio-tree nodes
+  parents.forEach(parent => {
+    const id = `${parent.__typename}/${parent.id}`
+
+    map.set(id, {
+      input: {
+        id: id,
+        name: "parent",
+        value: id,
+      },
+      label: parent.name,
+    });
+  });
+
+  // Build radio-tree node tree
+  parents.forEach(item => {
+    const id = `${item.__typename}/${item.id}`
+    const child = map.get(id);
+
+    if (!child) {
+      return;
+    }
+
+    if (item.parent) {
+      const parentId = `${item.parent.__typename}/${item.parent.id}`
+      const parent = map.get(parentId);
+
+      if (child && parent) {
+        (parent.nodes ??= []).push(child);
+      }
+    } else {
+      roots.push(child);
+    }
+  });
+
+  // disable nodes
+  const disableSubtree = (node: RadioTreeNode) => {
+    node.input.disabled = true;
+    node.nodes?.forEach(disableSubtree);
+  };
+
+  const rootToDisable = map.get(`Formation/${disabledId}`);
+  if (rootToDisable) {
+    disableSubtree(rootToDisable);
+  }
+
+  return roots;
+}
+
+// TODO
+// I think most of this can become an "area" selector.. like it'd be useful
+// when creating an area to select from this tree than it is to just type in
+// an id
+export default async (props: { params: Promise<{ id: string }> }) => {
+  const params = await props.params;
+  const id = parseInt(params.id);
+
+  const result = await query(GRAPHQL_ENDPOINT, dataQuery, null)
+    .then(r => r.json());
+  const { data, errors }:
+    {
+      data: {
+        potentialParentAreas: Area[],
+        potentialParentFormations: Formation[],
+      },
+      errors: any
+    } = result;
+
+  if (errors) {
+    console.error(errors);
+    return <div>There was an error generating the page.</div>
+  }
+
+  const { potentialParentAreas, potentialParentFormations } = data;
+
+  const action = async (data: FormData) => {
+    'use server';
+
+    const input = data.get('parent')?.toString() ?? '';
+
+    const match = input.match(/^([^/]+)\/(\d+)$/);
+
+    // Define the valid types
+    type ValidType = 'Area' | 'Formation';
+
+    const isValidType = (type: string): type is ValidType =>
+      type === 'Area' || type === 'Formation';
+
+    const parent = match && isValidType(match[1])
+      ? { __typename: match[1], id: parseInt(match[2], 10) }
+      : null;
+
+    await move(id, parent);
+
+    redirect(`/formations/${id}`);
+  }
+
+  const roots = [
+    ...buildTree([...potentialParentAreas, ...potentialParentFormations], id),
+    {
+      input: {
+        id: "node",
+        name: "parent",
+        value: "none",
+      },
+      label: "None",
+    }
+  ];
+
+  return (
+    <Form action={action}>
+      <RadioTree nodes={roots} />
+      <button type="submit">Move</button>
+    </Form>
+  )
+}
+
+
