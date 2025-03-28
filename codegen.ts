3d1b7b226835f74ced8820fc252990cf4ebc3e1d
Logan Grosz <logan.grosz@gmail.com>
2025-03-28 15:08:42 -0600

    build: avoid live server requirement
    This patch makes the live server effectively optional. When it is
    present, it will be used to generate both code and the schema. When a
    server is not available, code will be generated from the local schema.
    
    The schema should only be committed when it is stable.

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,45 @@
 
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
-const config: CodegenConfig = {
-  overwrite: true,
-  schema: "http://localhost:4000/graphql",
-  documents: ["**/*.tsx", "**/*.ts"],
-  generates: {
-    "gql/": {
-      preset: "client",
-      plugins: []
-    }
-  }
-};
+const GRAPHQL_URL = "http://localhost:4000/graphql";
+const SCHEMA_FILE = "./schema.graphql";
 
-export default config;
+async function schema() {
+  try {
+    const response = await fetch(GRAPHQL_URL, { method: 'HEAD' });
+
+    if (response.ok) {
+      console.warn("Using GraphQL server.")
+      return GRAPHQL_URL;
+    } else {
+      console.warn(`GraphQL server responed with "${response.statusText}" (${response.status}), falling back to local schema.`);
+      return SCHEMA_FILE;
+    }
+  } catch (error) {
+    console.warn("GraphQL server not available, falling back to local schema.");
+    return SCHEMA_FILE;
+  }
+}
+
+async function config() {
+  const config: CodegenConfig = {
+    overwrite: true,
+    schema: await schema(),
+    documents: ["**/*.tsx", "**/*.ts"],
+    generates: {
+      "gql/": {
+        preset: "client",
+        plugins: []
+      },
+    },
+  };
+
+  // generate new local schema
+  if (config.schema == GRAPHQL_URL) {
+    config.generates["schema.graphql"] = { plugins: ["schema-ast"] };
+  }
+
+  return config;
+}
+
+export default config();
