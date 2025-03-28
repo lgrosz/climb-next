7e2c616e69046ee88897f1a9e7e5972d2a07ebea
Logan Grosz <logan.grosz@gmail.com>
2025-03-27 23:28:16 -0600

    build: use graphql-codegen to generate schema
    Ensure the development server is running (by default on
    localhost:4000/graphql) and run `pnpm run graphql-codegen --watch`
    
    In an attempt to demystify things that I am not totally familiar this,
    this is how I interpret it working
    
    - graphql-request is the backbone of this.. it provides a simple (fetch
      based?) request api and types
    - graphql-codegen queries a live graphql server and generates more types
      (extending the base types provided by graphql-request) based on the
      queries that it scans from the project
      - unimportant, but the type gets applied to the query with some stupid
        map-lookup with the content of the query, then the variable is
        passed to a templated graphql-request wrapper function to make sure
        the type gets propagated down to the result.
    
    WIP:
      - how can I allow this to build regardless if graphql-codegen running
        first? I mean, I basically just bound the build of this application
        to a graphql server existing.. that's a lot of overhead for what I
        wanted to be a simple web application.
    
        I looks like I can generate a .schema file (probably with another
        command?) and commit it. So, maybe if the graphql server is present,
        use that, otherwise default to some .schema file.

--- graphql.ts
+++ graphql.ts
@@ -1,3 +1,13 @@
+import request from 'graphql-request'
+import { type TypedDocumentNode } from '@graphql-typed-document-node/core'
+
+export function graphqlQuery<TResult, TVariables>(
+  document: TypedDocumentNode<TResult, TVariables>,
+  ...[variables]: TVariables extends Record<string, never> ? [] : [TVariables]
+): Promise<TResult> {
+  return request('http://localhost:4000/graphql', document, variables ? variables : undefined);
+}
+
 export function query(endpoint: string, query: string, variables: any) {
   return fetch(endpoint, {
     method: "POST",
