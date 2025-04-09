aaeadc05af3d190f992da5e8ca7615f4922cce81
Logan Grosz <logan.grosz@gmail.com>
2025-04-09 15:42:35 -0600

    wip! feat: add edit topo page
    WIP:
      - fetch and serialize existing topo data
      - edit splines

---
+++ app/topos/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import TopoEditor from "@/components/TopoEditor";
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <TopoEditor />
+    </div>
+  );
+}
