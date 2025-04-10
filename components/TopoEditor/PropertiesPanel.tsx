da7f7e27336c6189ec0770db36a521d123f5499d
Logan Grosz <logan.grosz@gmail.com>
2025-04-09 23:08:06 -0600

    feat: add topo button in properties panel

--- components/TopoEditor/PropertiesPanel.tsx
+++ components/TopoEditor/PropertiesPanel.tsx
@@ -1,15 +1,34 @@
 import PropertyInput from './PropertyInput';
-import { useTopoEditor } from '../context/TopoEditorContext';
+import { SplineReducerActionType, useTopoEditor } from '../context/TopoEditorContext';
+import { BasisSpline } from '@/lib/BasisSpline';
+import { useEffect } from 'react';
 
 export default function PropertiesPanel() {
-  const { title, setTitle } = useTopoEditor();
+  const {
+    title,
+    setTitle,
+    splines,
+    setSplines,
+  } = useTopoEditor();
+
+  const addSpline = () => {
+    setSplines({
+      type: SplineReducerActionType.Add,
+      spline: new BasisSpline,
+    })
+  }
+
+  useEffect(() => {
+    console.log('splines changed', splines);
+  }, [splines]);
 
   return (
     <div className="w-80 bg-white border-l p-4 overflow-y-auto">
       <h2 className="text-lg font-semibold mb-4">Properties</h2>
       <div className="space-y-4">
         <PropertyInput label="Title" value={title} onChange={setTitle} />
-        {/* Add more PropertyInput components here as necessary */}
+        { /* TODO I'd rather have a context menu to add a spline that would start some interactive spline creator */}
+        <button onClick={addSpline}>Add spline</button>
       </div>
     </div>
   );
