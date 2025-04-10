c6c26ab8066d8c2394d3e00d7cd93580e7d68845
Logan Grosz <logan.grosz@gmail.com>
2025-04-09 21:04:39 -0600

    feat: add spline reducer to topo context

--- components/context/TopoEditorContext.tsx
+++ components/context/TopoEditorContext.tsx
@@ -1,8 +1,11 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import { BasisSpline } from "@/lib/BasisSpline";
+import React, { createContext, useContext, useState, ReactNode, useReducer } from "react";
 
 interface TopoEditorContextType {
   title: string;
   setTitle: React.Dispatch<React.SetStateAction<string>>;
+  splines: BasisSpline[],
+  setSplines: React.ActionDispatch<[action: SplineReducerAction]>,
 }
 
 const TopoEditorContext = createContext<TopoEditorContextType | undefined>(undefined);
@@ -19,11 +22,59 @@ interface TopoEditorProviderProps {
   children: ReactNode;
 }
 
+export enum SplineReducerActionType {
+  Add,
+  Remove,
+  Update,
+}
+
+interface SplineReducerAddSpline {
+  type: SplineReducerActionType.Add;
+  spline: BasisSpline;
+}
+
+interface SplineReducerRemoveSpline {
+  type: SplineReducerActionType.Remove;
+  index: number;
+}
+
+interface SplineReducerUpdateSpline {
+  type: SplineReducerActionType.Update;
+  index: number;
+  spline: BasisSpline;
+}
+
+export type SplineReducerAction =
+  | SplineReducerAddSpline
+  | SplineReducerRemoveSpline
+  | SplineReducerUpdateSpline;
+
+function splinesReducer(state: BasisSpline[], action: SplineReducerAction) {
+  switch(action.type) {
+    case SplineReducerActionType.Add:
+      return [...state, action.spline];
+    case SplineReducerActionType.Remove:
+      return [...state.slice(0, action.index), ...state.slice(action.index + 1)]
+    case SplineReducerActionType.Update:
+      return [...state.slice(0, action.index), action.spline, ...state.slice(action.index + 1)]
+  }
+}
+
 export const TopoEditorProvider = ({ children }: TopoEditorProviderProps) => {
   const [title, setTitle] = useState("");
+  const [splines, setSplines] = useReducer(splinesReducer, []);
 
   return (
-    <TopoEditorContext.Provider value={{ title, setTitle }}>
+    <TopoEditorContext.Provider
+      value={
+        {
+          title,
+          setTitle,
+          splines,
+          setSplines,
+        }
+      }
+    >
       {children}
     </TopoEditorContext.Provider>
   );
