e53deb602a06c85809a1c8b67d35969ca50a5afe
Logan Grosz <logan.grosz@gmail.com>
2025-05-18 11:17:41 -0600

    refactor: add type property to property-input component
    This property directly reflects the HTML input type attribute.

--- components/TopoEditor/PropertyInput.tsx
+++ components/TopoEditor/PropertyInput.tsx
@@ -1,15 +1,18 @@
+import { HTMLInputTypeAttribute } from "react";
+
 type PropertyInputProps = {
   label: string;
   value: string;
+  type: HTMLInputTypeAttribute;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-export default function PropertyInput({ label, value, onChange }: PropertyInputProps) {
+export default function PropertyInput({ label, type, value, onChange }: PropertyInputProps) {
   return (
     <div>
       <label className="block text-sm font-medium mb-1">{label}</label>
       <input
-        type="text"
+        type={type}
         value={value}
         className="w-full"
         onChange={onChange}
