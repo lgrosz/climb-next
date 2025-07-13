afce7ab5d40330af92e79a19cb8ea2b46f293256
Logan Grosz <logan.grosz@gmail.com>
2025-07-13 16:06:31 -0600

    feat: inject images into topo editor from edit page
    Queries for the already present images in the topo and adds them as
    images to the editor.

--- app/topos/[id]/edit/page.tsx
+++ app/topos/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-import { TopoWorld } from "@/components/context/TopoWorld";
+import { TopoWorld, Image } from "@/components/context/TopoWorld";
 import TopoEditor from "@/components/TopoEditor/TopoEditor";
 import { graphql } from "@/gql";
 import { graphqlQuery } from "@/graphql";
@@ -9,6 +9,14 @@ const QUERY = graphql(`
         title
         height
         width
+        features {
+          __typename
+          ... on TopoImageFeature {
+            image { id alt }
+            dest { min { x y } max { x y } }
+            source { min { x y } max { x y } }
+          }
+        }
       }
     }
 `);
@@ -22,13 +30,29 @@ export default async function Page(
   const {
     title,
     width,
-    height
+    height,
+    features,
   } = data.topo;
+
+  const images: Image[] = features
+    .filter(f => f.__typename === "TopoImageFeature")
+    .map(f => ({
+      id: f.image.id,
+      alt: f.image.alt ?? undefined,
+      dest: {
+        min: { x: f.dest.min.x, y: f.dest.min.y },
+        max: { x: f.dest.max.x, y: f.dest.max.y }
+      },
+      source: f.source ? {
+        min: { x: f.source.min.x, y: f.source.min.y },
+        max: { x: f.source.max.x, y: f.source.max.y }
+      } : undefined,
+    }));
 
   const world: TopoWorld = {
       title: title ?? "",
       lines: [],
-      images: [],
+      images,
       size: { width, height },
   };
 
