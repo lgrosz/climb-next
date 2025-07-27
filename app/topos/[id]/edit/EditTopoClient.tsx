74ae6b2912c8091bad5d65f56a41ce445637d971
Logan Grosz <logan.grosz@gmail.com>
2025-07-27 17:14:43 -0600

    feat: send title mutation when done editing topo

--- app/topos/[id]/edit/EditTopoClient.tsx
+++ app/topos/[id]/edit/EditTopoClient.tsx
@@ -1,8 +1,48 @@
 "use client";
 
-import { TopoSessionProvider } from "@/components/context/TopoSession";
+import { TopoSessionProvider, useTopoSession } from "@/components/context/TopoSession";
 import { TopoWorld, TopoWorldProvider } from "@/components/context/TopoWorld";
 import TopoEditor from "@/components/TopoEditor/TopoEditor";
+import { TopoChange } from "@/hooks/useTopoHistory";
+import { title } from "@/topos/actions";
+import { useParams } from "next/navigation";
+import { useCallback } from "react";
+
+function EditTopoClientInner({
+  id,
+}: {
+  id: string,
+}) {
+  const { changes } = useTopoSession();
+
+  const changeToAction = useCallback((change: TopoChange) => {
+    const action = change.action;
+
+    switch (action.type) {
+      case "title":
+        return () => title(id, action.title)
+    }
+
+    return async () => {
+      console.log("Will not apply", change);
+    };
+  }, [id]);
+
+  const finish = useCallback(async () => {
+    const actions = changes
+      .map(changeToAction);
+
+    for (const action of actions) {
+      await action();
+    }
+  }, [changes, changeToAction]);
+
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <TopoEditor onFinish={finish} />
+    </div>
+  );
+}
 
 export default function EditTopoClient({
     availableClimbs,
@@ -14,14 +54,14 @@ export default function EditTopoClient({
     }[],
     world: TopoWorld,
 }) {
+  const { id } = useParams<{ id: string }>();
+
   return (
-    <div className="w-screen h-screen overflow-hidden">
-      <TopoWorldProvider initial={world}>
-        <TopoSessionProvider availableClimbs={availableClimbs} >
-          <TopoEditor />
-        </TopoSessionProvider>
-      </TopoWorldProvider>
-    </div>
+    <TopoWorldProvider initial={world}>
+      <TopoSessionProvider availableClimbs={availableClimbs} >
+        <EditTopoClientInner id={id} />
+      </TopoSessionProvider>
+    </TopoWorldProvider>
   );
 }
 
