27f87510b23edd6250d63ae882d836fb0971b1ad
Logan Grosz <logan.grosz@gmail.com>
2025-07-27 20:43:22 -0600

    test: prefer babel-jest to ts-jest
    This seems to work better as transforming tsx files.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+\.(j|t)sx?$": ["babel-jest",{}],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
   },
 };
