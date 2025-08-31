3973d9b39b3065ae9ad03b91a536facfa578841b
Logan Grosz <logan.grosz@gmail.com>
2025-08-31 14:14:20 -0600

    fix: remove all ascent references
    This is no longer provided by the GraphQL schema.

--- app/climbs/[id]/page.tsx
+++ app/climbs/[id]/page.tsx
@@ -4,8 +4,6 @@ import { graphqlQuery } from '@/graphql';
 import FontainebleauGrade from '@/fontainebleau-grade';
 import YosemiteDecimalGrade from '@/yosemite-decimal-grade';
 import { graphql } from '@/gql';
-import { DateInterval } from '@/date-interval';
-import { BoundType } from '@/bound';
 
 const climbData = graphql(`
   query climbData($id: ID!) {
@@ -32,110 +30,9 @@ const climbData = graphql(`
         ... on Area { id name }
         ... on Formation { id name }
       }
-      ascents {
-        id
-        climber {firstName lastName }
-        dateWindow
-        grades {
-          ... on Fontainebleau {
-            __typename
-            font_value: value
-          }
-          ... on Vermin {
-            __typename
-            v_value: value
-          }
-          ... on YosemiteDecimal {
-            __typename
-            yds_value: value
-          }
-        }
-      }
     }
   }
 `);
-
-interface AscentRowProps {
-  id: string,
-  climber: {
-    firstName: string,
-    lastName: string,
-  }
-  dateWindow?: string | null
-  grades: {
-    __typename: string,
-    font_value?: string
-    v_value?: string
-    yds_value?: string
-  }[]
-}
-
-function intervalDateString(dates: DateInterval | null) {
-  if (
-    !dates ||
-    (dates.upper.type === BoundType.Unbounded && dates.lower.type === BoundType.Unbounded)
-  ) {
-    return "";
-  }
-
-  // TODO
-  // - nicer strings like...
-  //   - "Winter 2024 - Spring 2025"
-  //   - 1990s
-  //   - Mid February 2024
-  // - Since I am taking Bounds, I should really take into account the included/excluded thing
-
-  if (dates.upper.type === BoundType.Unbounded && dates.lower.type !== BoundType.Unbounded) {
-    return `After ${dates.lower.value.toLocaleDateString()}`
-  }
-
-  if (dates.lower.type === BoundType.Unbounded && dates.upper.type !== BoundType.Unbounded) {
-    return `Before ${dates.upper.value.toLocaleDateString()}`
-  }
-
-  if (dates.lower.type !== BoundType.Unbounded && dates.upper.type !== BoundType.Unbounded) {
-    if (dates.lower.value.getTime() === dates.upper.value.getTime()) {
-      return dates.lower.value.toLocaleDateString();
-    }
-
-    return `Between ${dates.lower.value.toLocaleDateString()} and ${dates.upper.value.toLocaleDateString()}`
-  }
-
-  // NOTE this is not possible, I think Typescript compiler is doing the checking incorrectly, I shouldn't some of the above checks.
-  return "-";
-}
-
-function AscentRow(props: AscentRowProps) {
-  const fontGrades = props.grades.map(grade =>
-    grade.__typename == "Fontainebleau" && grade.font_value ? FontainebleauGrade.fromString(grade.font_value) : undefined
-  ).filter((g): g is FontainebleauGrade => !!g).sort(FontainebleauGrade.compare);
-
-  const verminGrades = props.grades.map(grade =>
-    grade.__typename == "Vermin" && grade.v_value ? VerminGrade.fromString(grade.v_value) : undefined
-  ).filter((g): g is VerminGrade => !!g).sort(VerminGrade.compare);
-
-  const ydsGrades = props.grades.map(grade =>
-    grade.__typename == "YosemiteDecimal" && grade.yds_value ? YosemiteDecimalGrade.fromString(grade.yds_value) : undefined
-  ).filter((g): g is YosemiteDecimalGrade => !!g).sort(YosemiteDecimalGrade.compare);
-
-  const gradeString = [
-    FontainebleauGrade.slashString(fontGrades),
-    VerminGrade.slashString(verminGrades),
-    YosemiteDecimalGrade.slashString(ydsGrades),
-  ].filter(s => s).join(", ");
-
-  const dateInterval = props.dateWindow ? DateInterval.fromISOString(props.dateWindow) : null;
-  const dateString = intervalDateString(dateInterval);
-
-  return (
-    <tr>
-      <td>{props.climber.firstName} {props.climber.lastName}</td>
-      <td>{dateString}</td>
-      <td>{gradeString ? gradeString : "-" }</td>
-      <td><Link href={`/ascents/${props.id}`}>Read more</Link></td>
-    </tr>
-  );
-}
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -164,8 +61,6 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const ydsGrades = climb.grades.map(grade =>
     grade.__typename == "YosemiteDecimal" && grade.yds_value ? YosemiteDecimalGrade.fromString(grade.yds_value) : undefined
   ).filter((g): g is YosemiteDecimalGrade => !!g).sort(YosemiteDecimalGrade.compare);
-
-  const ascents = climb.ascents;
 
   return (
     <div>
@@ -210,22 +105,6 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         )}
         </ul>
       )}
-      <h3>Ascents</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Climber</th>
-            <th>Date</th>
-            <th>Suggested Grade</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ascents.map(ascent => (
-            <AscentRow key={`ascent-${ascent.id}`} {...ascent}/>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 }
