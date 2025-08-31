d13ce2639670b30288167b50257f8d286c4ff019
Logan Grosz <logan.grosz@gmail.com>
2025-08-31 13:51:59 -0600

    build(codegen): add switch for gql/ generation

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,11 @@
 
 import type { CodegenConfig } from '@graphql-codegen/cli';
 import { GRAPHQL_ENDPOINT } from './constants'
+import yn from 'yn';
 
 const SCHEMA_FILE = "./schema.graphql";
+
+const GQL_GENERATE = yn(process.env.GQL_GENERATE || "y");
 
 async function schema() {
   try {
@@ -26,34 +29,36 @@ async function config() {
     overwrite: true,
     schema: await schema(),
     documents: ["**/*.tsx", "**/*.ts"],
-    generates: {
-      "gql/": {
-        preset: "client",
-        plugins: [],
-        config: {
-          scalars: {
-            DateInterval: {
-              input: 'string',
-              output: 'string',
-            },
-            FontainebleauGrade: {
-              input: 'string',
-              output: 'string',
-            },
-            VerminGrade: {
-              input: 'string',
-              output: 'string',
-            },
-            YosemiteDecimalGrade: {
-              input: 'string',
-              output: 'string',
-            },
-          },
-          strictScalars: true,
-        },
-      },
-    },
+    generates: { },
   };
+
+  if (GQL_GENERATE) {
+    config.generates["gql/"] = {
+      preset: "client",
+      plugins: [],
+      config: {
+        scalars: {
+          DateInterval: {
+            input: 'string',
+            output: 'string',
+          },
+          FontainebleauGrade: {
+            input: 'string',
+            output: 'string',
+          },
+          VerminGrade: {
+            input: 'string',
+            output: 'string',
+          },
+          YosemiteDecimalGrade: {
+            input: 'string',
+            output: 'string',
+          },
+        },
+        strictScalars: true,
+      },
+    };
+  }
 
   // generate new local schema
   if (config.schema == GRAPHQL_ENDPOINT) {
