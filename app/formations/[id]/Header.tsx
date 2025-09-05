a9a3fe3af481a61ef19f30e89055a4fb6de3dfc7
Logan Grosz <logan.grosz@gmail.com>
2025-09-04 23:47:20 -0600

    refactor: component-ize editable header
    Introduces `EditableHeader` and uses it as the formation name header.
    This component should make it easier to introduce editable headers for
    other entity pages, or even multiple editable headers on the same page.

--- app/formations/[id]/Header.tsx
+++ app/formations/[id]/Header.tsx
@@ -1,65 +1,24 @@
 "use client";
 
+import EditableHeader from "@/components/EditableHeader";
 import { rename } from "@/formations/actions";
-import { useToggle } from "@/hooks/useToggle";
-import { useFormStatus } from "react-dom";
-
-function RenameForm({
-  id,
-  name,
-  onCancel
-}: {
-  id: string,
-  name?: string,
-  onCancel?: () => void,
-}) {
-  const { pending } = useFormStatus();
-  return (
-    <div className="w-full p-4 flex flex-col-reverse sm:flex-row sm:items-center sm:justify-between gap-2">
-      <input type="hidden" name="id" value={id} disabled={pending} />
-      <input className="w-full" type="text" name="name" defaultValue={name} placeholder="Formation name" disabled={pending} />
-      <div className="flex gap-2 flex-row">
-        { onCancel && <button onClick={onCancel} disabled={pending}>Cancel</button> }
-        <input type="submit" value="Save" disabled={pending} />
-      </div>
-    </div>
-  );
-}
 
 export default function Header(
   { id, name }: { id: string, name?: string }
 ) {
-  const [editing, toggleEditing] = useToggle();
-
-  const action = async (formData: FormData) => {
-    const id = formData.get("id")?.toString() ?? "";
-    const name = formData.get("name")?.toString() ?? "";
+  const action = async (name: string) => {
     await rename(id, name);
-
-    // TODO
-    // - on success, set the name
-    // - on error, display error
-
-    toggleEditing();
+    // TODO handle result
   };
 
-  if (editing) {
-    return (
-      <form action={action}>
-        <RenameForm
-          id={id}
-          name={name}
-          onCancel={toggleEditing}
-        />
-      </form>
-    );
-  }
-
-  return (
-    <div className="w-full p-4 flex flex-col-reverse sm:flex-row sm:items-center sm:justify-between gap-2">
-      <div className="flex-1"><h1>{name ?? <i>Anonymous Formation</i>}</h1></div>
-      <div className="flex gap-2 flex-wrap"><button onClick={toggleEditing}>Edit</button></div>
-    </div>
+  return(
+    <EditableHeader
+      action={action}
+      as="h1"
+      value={name}
+      emptyValue="Anonymous Formation"
+      placeholder="Enter formation name"
+    />
   );
 }
 
