500abd0ab1b8a5c414d7487949277a4c4d2ae3e9
Logan Grosz <logan.grosz@gmail.com>
2025-09-30 11:07:28 -0600

    feat: create sector directly from region page
    Include refactors to make the sector form respond to query parameters,
    at least for the crag.

--- app/sectors/new/page.tsx
+++ app/sectors/new/page.tsx
@@ -1,20 +1,8 @@
-import { FragmentType, getFragmentData, graphql } from "@/gql"
+import { graphql } from "@/gql"
 import { create } from "@/sectors/actions";
 import { redirect } from "next/navigation";
 import { graphqlQuery } from "@/graphql";
-
-const CragFieldsFragment = graphql(`
-  fragment NewSectorCragFields on Crag {
-    id name
-  }
-`);
-
-const RegionFieldsFragment = graphql(`
-  fragment NewSectorRegionFields on Region {
-    name
-    crags { id ...NewSectorCragFields }
-  }
-`);
+import SectorForm from "./SectorForm";
 
 const cragOptions = graphql(`
   query cragOptions {
@@ -22,30 +10,6 @@ const cragOptions = graphql(`
     regions { id ...NewSectorRegionFields }
   }
 `);
-
-function CragItem(frag: FragmentType<typeof CragFieldsFragment>) {
-  const {
-    id,
-    name
-  } = getFragmentData(CragFieldsFragment, frag);
-  return (
-    <option id={id} label={name ?? ""} value={id} />
-  );
-}
-
-function RegionItem(frag: FragmentType<typeof RegionFieldsFragment>) {
-  const {
-    name,
-    crags
-  } = getFragmentData(RegionFieldsFragment, frag);
-  return (
-    <optgroup label={name ?? ""}>
-      {crags.map(c => (
-	<CragItem key={c.id} {...c} />
-      ))}
-    </optgroup>
-  );
-}
 
 export default async function Page()
 {
@@ -66,55 +30,7 @@ export default async function Page()
   return (
     <div>
       <h1>Create a new sector</h1>
-      <form action={action}>
-	<div>
-	  <label
-	    className="block"
-	    htmlFor="name"
-	  >
-	    Add a name
-	  </label>
-	  <input
-	    id="name"
-	    type="text"
-	    className="w-full"
-	    name="name"
-	  />
-	  <label
-	    className="block"
-	    htmlFor="description"
-	  >
-	    Add a description
-	  </label>
-	  <textarea
-	    id="description"
-	    className="w-full resize-y"
-	    name="description"
-	  />
-	  <label
-	    className="block"
-	    htmlFor="crag"
-	  >
-	    Select a crag
-	  </label>
-	  <select required name="crag">
-	    {crags.map(c => (
-	      <CragItem key={c.id} {...c} />
-	    ))}
-	    {regions.map(r => (
-	      <RegionItem key={r.id} {...r} />
-	    ))}
-	  </select>
-	</div>
-	<div className="flex justify-end">
-	  <button type="button">
-	    Cancel
-	  </button>
-	  <button type="submit">
-	    Create
-	  </button>
-	</div>
-      </form>
+      <SectorForm action={action} crags={crags} regions={regions} />
     </div>
   );
 }
