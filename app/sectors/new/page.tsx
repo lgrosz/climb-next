4f3378e8fe4cc306da3a2e38b4458acc60705966
Logan Grosz <logan.grosz@gmail.com>
2025-10-06 12:15:49 -0600

    refactor: export potentially shared sector creation code

--- app/sectors/new/page.tsx
+++ app/sectors/new/page.tsx
@@ -1,36 +1,37 @@
 import { graphql } from "@/gql"
-import { create } from "@/sectors/actions";
 import { redirect } from "next/navigation";
 import { graphqlQuery } from "@/graphql";
 import SectorForm from "./SectorForm";
+import { toSectorParentOptions } from "./SectorParentOptions";
+import { submitNewSectorForm } from "./actions";
 
-const cragOptions = graphql(`
-  query cragOptions {
-    crags { id ...NewSectorCragFields }
-    regions { id ...NewSectorRegionFields }
+const query = graphql(`
+  query NewSectorPageData {
+    crags { id ...CragSectorParentOptionFields }
+    regions { id ...RegionSectorParentOptionFields }
   }
 `);
 
 export default async function Page()
 {
-  const { crags, regions } = await graphqlQuery(cragOptions);
+  const result = await graphqlQuery(query);
+  const parentOptions = toSectorParentOptions(result);
 
   const action = async (formData: FormData) => {
     "use server";
-
-    const name = formData.get("name")?.toString();
-    const description = formData.get("description")?.toString();
-    const crag_id = formData.get("crag")?.toString();
-    if (!crag_id) throw new Error("Please select a crag");
-
-    const id = await create(name, description, crag_id);
+    const id = await submitNewSectorForm(formData);
     redirect(`/sectors/${id}`);
   }
 
   return (
     <div>
       <h1>Create a new sector</h1>
-      <SectorForm action={action} crags={crags} regions={regions} />
+      <SectorForm id="new-sector-form" action={action} parentOptions={parentOptions} />
+      <div className="flex justify-end">
+        <button form="new-sector-form" type="submit">
+          Create
+        </button>
+      </div>
     </div>
   );
 }
