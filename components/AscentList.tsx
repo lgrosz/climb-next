3977276ef0ccad8b40492e9975151e947f216c7f
Logan Grosz <logan.grosz@gmail.com>
2025-10-30 17:52:21 -0600

    feat: select ascents in table to remove ascents

--- components/AscentList.tsx
+++ components/AscentList.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { AscentTable } from "./AscentTable";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
 
 export default function AscentList({
   climbId,
@@ -12,9 +12,25 @@ export default function AscentList({
   ascents: ComponentProps<typeof AscentTable>["ascents"]
 })
 {
+  const [selectedAscents, setSelectedAscents] = useState<Set<string>>(new Set);
+
+  const toggleSelection = (id: string) => {
+    setSelectedAscents(prev => {
+      const next = new Set(prev);
+      if (next.has(id)) next.delete(id);
+      else next.add(id);
+      return next;
+    });
+  };
+
   return (
     <div>
-      <AscentTable ascents={ascents} className="w-full" />
+      <AscentTable
+        ascents={ascents}
+        selected={selectedAscents}
+        toggleSelect={toggleSelection}
+        className="w-full"
+      />
       <div className="flex justify-end">
         { !!climbId &&
           <Link href={`/climbs/${climbId}/add-ascent`}>Add ascent</Link>
